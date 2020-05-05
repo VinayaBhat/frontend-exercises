fbcaf65ed7d04df9d4dfb2076a86954b592b0931
VinayaBhat <vibha2@pdx.edu>
2020-05-04 17:53:40 -0700

    url control

--- assign3/2-url.js
+++ assign3/2-url.js
@@ -10,44 +10,59 @@
 // human: no
 
 
-var output=document.getElementById("result");
-output.style.display="none";
+var output = document.getElementById("result");
+output.style.display = "none";
 
 function url_submitted(event) {
-    event.preventDefault();    
-    var str = document.getElementById("comments").value;  
-    if(ValidURL(str)){ 
-    var decodedurl=decodeURIComponent(str);
-    
-    var url = decodedurl.split('?')[0];
-    var url_element=document.getElementById("url");
-    url_element.innerHTML=url; 
+    event.preventDefault();
+    var str = document.getElementById("comments").value;
+    //to avoid empty string on submit
+    if (ValidURL(str)) {
+        var decodedurl = decodeURIComponent(str);
 
-    var parameter=document.getElementById("params");
-    var firstChild = parameter.firstElementChild; 
-        while (firstChild) { 
-            firstChild.remove(); 
-            firstChild = parameter.firstElementChild; 
-        } 
+        if (decodedurl.includes('?')) {
+            console.log("yo")
+            var url = decodedurl.split('?')[0];
+            var url_element = document.getElementById("url");
+            url_element.innerHTML = url;
+            var parameter = document.getElementById("params");
+            delete_parameters(parameter);               
+            let params = decodedurl.split('?')[1].split('&');
+            if (params && params.length) {
+                params.map(keyValue => {
+                    let keyValuePair = keyValue.split('=')
+                    let key = keyValuePair[0];
+                    let value = keyValuePair[1];
+                    var p_element = document.createElement("p");
+                    p_element.className = "mt-2 mb-2";
+                    p_element.innerHTML = key + ": " + value;
+                    parameter.appendChild(p_element);
+                })
+            }
 
-    let params = decodedurl.split('?')[1].split('&');
-    if (params && params.length) {
-        params.map(keyValue => {
-            let keyValuePair = keyValue.split('=')
-            let key = keyValuePair[0];
-            let value = keyValuePair[1];
-            var p_element=document.createElement("p");
-            p_element.className="mt-2 mb-2";
-            p_element.innerHTML=key+": "+value;
-            parameter.appendChild(p_element);
-        })
+        } else {
+            var url_element = document.getElementById("url");
+            url_element.innerHTML = decodedurl;
+            var parameter = document.getElementById("params");
+            delete_parameters(parameter);              
+        }
+        output.style.display = "block";
+    }else{
+        output.style.display = "none"; 
     }
-    output.style.display="block";
-}
- 
+
 }
 
 function ValidURL(string) {
     var url = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
     return (url !== null)
-  };
+};
+
+function delete_parameters(parameter){
+    
+    var firstChild = parameter.firstElementChild;
+    while (firstChild) {
+        firstChild.remove();
+        firstChild = parameter.firstElementChild;
+    }
+}
