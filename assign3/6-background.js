04a1d2f74df311d54aa22cd982350475f8e587ad
VinayaBhat <vibha2@pdx.edu>
2020-05-06 00:12:42 -0700

    background

--- assign3/6-background.js
+++ assign3/6-background.js
@@ -1,1 +1,30 @@
 // Enter your code here
+
+document.body.addEventListener("load", changeColor());
+
+var timeout;
+function changeColor() {
+    var interval = document.getElementById("interval").value;
+    timeout = setInterval(
+        function () {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            document.body.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";;
+        }, interval * 1000);
+}
+
+function startstop_toggle() {
+    event.preventDefault();
+    var button = document.getElementById("submit");
+    if (button.value == "Stop") {
+        button.value = "Start";
+        document.getElementById("interval").disabled = false;
+        clearInterval(timeout);
+    } else {
+        button.value = "Stop";
+        document.getElementById("interval").disabled = true;
+        clearInterval(timeout);
+        changeColor();
+    }
+}
