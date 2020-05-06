4463c4d71b6f63dde49a83feb460101472519478
VinayaBhat <vibha2@pdx.edu>
2020-05-05 21:03:20 -0700

    palindrome

--- assign3/5-palindrome.js
+++ assign3/5-palindrome.js
@@ -1,1 +1,39 @@
 // Enter your code here
+function palindrome(number) {
+    var remainder, reverse_Num = 0;
+    var num_Copy = number;
+    while (number > 0) {
+        remainder = number % 10;
+        number = Math.floor(number / 10);
+        reverse_Num = reverse_Num * 10 + remainder;
+    }
+    if (reverse_Num == num_Copy) {
+        return true;
+    }
+    return false;
+}
+
+function checkPositive(number) {
+    if (number > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+function checkforPalindrome(event) {
+    var number = document.getElementById("number").value;
+    var result = document.getElementById("result");
+    if (checkPositive(number)) {
+        if (palindrome(number)) {
+            result.innerHTML = "Yes";
+        } else {
+            result.innerHTML = "No";
+        }
+    } else {
+        result.innerHTML = "No";
+    }
+}
+
